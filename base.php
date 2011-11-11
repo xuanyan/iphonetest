4f3004d6281c46b76498d00b46bbc2aedf30eb75
xuanyan <xuanyan1983@gmail.com>
2011-11-11 16:08:55 +0800

    xxx

---
+++ base.php
@@ -0,0 +1,44 @@
+<?php
+
+/*
+ * This file is part of the Geek-Zoo Projects.
+ *
+ * @copyright (c) 2010 Geek-Zoo Projects More info http://www.geek-zoo.com
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License
+ * @author xuanyan <xuanyan@geek-zoo.com>
+ *
+ */
+
+abstract class base_abstract
+{
+    function __construct()
+    {
+        $this->view = new Templite(ROOT_PATH.'/template');
+        $this->db = Database::connect('pdo', 'sqlite:'.ROOT_PATH.'./db.sqlite');
+    }
+
+    function isPost()
+    {
+        return 'POST' == @$_SERVER['REQUEST_METHOD'];
+    }
+
+    function redirect($url = '')
+    {
+        $site = SITE_URL;
+        if (!$url) {
+            $url = isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $site;
+        }
+
+        if (substr($url, 0, 4) != 'http') {
+            if ($url{0} != '/') {
+                $url = '/'.$url;
+            }
+            $url = $site.$url;
+        }
+        header('Location: ' . $url);
+        exit;
+    }
+}
+
+
+?>
