f776cd753e9a4c90e81c0b8f9db6d8ed80adacb3
xuanyan <xuanyan1983@gmail.com>
2011-11-11 14:17:56 +0800

    uu

---
+++ Templite.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Geek-Zoo Projects.
+ *
+ * @copyright (c) 2010 Geek-Zoo Projects More info http://www.geek-zoo.com
+ * @license http://opensource.org/licenses/gpl-2.0.php The GNU General Public License
+ * @author xuanyan <xuanyan@geek-zoo.com>
+ *
+ */
+
+class Templite
+{
+    private $val = array();
+    private $template_dir = '';
+
+    function __construct($template_dir)
+    {
+        $this->template_dir = $template_dir;
+    }
+
+    function assign($key, $val)
+    {
+        $this->val[$key] = $val;
+    }
+
+    function display($file)
+    {
+        extract($this->val);
+        require $this->template_dir.'/'.$file;
+    }
+}
+
+
+
+?>
